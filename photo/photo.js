5054f67b0a8c1a3875f270dd60f8ef7c33a6ddf5
Pluggito <Pluggito@example.com>
2024-10-01 12:44:53 +0100

    small screens commit

--- photo/photo.js
+++ photo/photo.js
@@ -17,3 +17,20 @@ document.body.addEventListener("click",(event)=> {
     imgs.forEach((img) => img.classList.remove("open"));
     clickedElement.classList.add("open");
 });
+
+
+/*--------------------sidemenu for 320px screens----------- */
+
+
+// Function to toggle the menu visibility
+function toggleMenu() {
+    const menu = document.getElementById('sidemenu'); 
+    menu.classList.toggle('show-menu'); // Toggle the class to show or hide the menu
+}
+
+// Add an event listener to the toggle icon
+document.querySelector('.toggle-menu').addEventListener('click', toggleMenu);
+
+
+
+
